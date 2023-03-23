9a171e9e5ccbe88f3aa22e645731b07746a59bb7
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-03-23 17:45:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const path = require('path')
+const http = require('http');
 
-const PORT = process.env.PORT || 5001
+const port = 9000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
